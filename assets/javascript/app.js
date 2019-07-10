2ae5b0a4cbbac74b6892d3ec55311a340f6ff7c4
Christina <christinachon90@gmail.com>
2019-07-09 21:04:01 -0700

    added calculation on train times

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -20,6 +20,8 @@ var firebaseConfig = {
 $("#current-time").text(moment().format(" hh :  mm a , MMMM D YYYY"));
 
 $("#add-train").click(function(){
+  event.preventDefault();
+
   let name = $("#train-name").val().trim();
   let destination = $("#destination").val().trim();
   let firstTrain = $("#first-train").val().trim();
@@ -30,26 +32,30 @@ $("#add-train").click(function(){
     destination: destination,
     firstTrain: firstTrain,
     frequency: frequency,
-  })
+  });
+
+  $(".form-control").val("");
 }) 
 
 database.ref().on("child_added", function(snapshot){
   let newTrain = snapshot.val();
+  let firstTrainNew = moment(snapshot.val().firstTrain, "hh:mm").subtract(1, "years");
+  let diffTime = moment().diff(moment(firstTrainNew), "minutes");
+  let remainder = diffTime % snapshot.val().frequency;
+  var minAway = snapshot.val().frequency - remainder;
+  var nextTrain = moment().add(minAway, "minutes");
+  nextTrain = moment(nextTrain).format("hh:mm");
   let newRow = $("<tr>");
   let newName = $("<td>");
   newName.append(newTrain.name);
-  
-  let newDestination = $("<td>");
+    let newDestination = $("<td>");
   newDestination.append(newTrain.destination);
-  
-  let newFirstTrain = $("<td>");
-  newFirstTrain.append(newTrain.firstTrain);
-  
-  let newFrequency = $("<td>");
+    let newFirstTrain = $("<td>");
+  newFirstTrain.append(nextTrain);
+    let newFrequency = $("<td>");
   newFrequency.append(newTrain.frequency);
-  
-  let newMinute = $("<td>");
-  newMinute.append("-");
+    let newMinute = $("<td>");
+  newMinute.append(minAway);
 
   newRow.append(newName, newDestination, newFrequency, newFirstTrain, newMinute);
   $("#add-train-row").append(newRow);
