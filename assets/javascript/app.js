bba24180983362c75a7bc25dc7d97c7d78b9b6e0
Christina <christinachon90@gmail.com>
2019-07-09 20:04:27 -0700

    adds new row of trains on submit button

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,5 @@
-$(document).ready(function(){
-  var firebaseConfig = {
+$(document).ready(function () {
+var firebaseConfig = {
     apiKey: "AIzaSyAigYuNDsmseMJv5ZUo1k0hPqmHoSPQCrY",
     authDomain: "train-schedule-49fb3.firebaseapp.com",
     databaseURL: "https://train-schedule-49fb3.firebaseio.com",
@@ -18,5 +18,37 @@ $(document).ready(function(){
   var firstTrain;
   var frequency = 0;
 
+$("#add-train").click(function(){
+  let name = $("#train-name").val().trim();
+  let destination = $("#destination").val().trim();
+  let firstTrain = $("#first-train").val().trim();
+  let frequency = $("#frequency").val().trim();
 
+  database.ref().push().set({
+    name: name,
+    destination: destination,
+    firstTrain: firstTrain,
+    frequency: frequency,
+  })
+}) 
+
+database.ref().on("child_added", function(snapshot){
+  let newTrain = snapshot.val();
+  let newRow = $("<tr>");
+  let newName = $("<td>");
+  newName.append(newTrain.name);
+  let newDestination = $("<td>");
+  newDestination.append(newTrain.destination);
+  let newFirstTrain = $("<td>");
+  newFirstTrain.append(newTrain.firstTrain);
+  let newFrequency = $("<td>");
+  newFrequency.append(newTrain.frequency);
+  let newMinute = $("<td>");
+  newMinute.append("-");
+
+  newRow.append(newName, newDestination, newFrequency, newFirstTrain, newMinute);
+  $("#add-train-row").append(newRow);
+  console.log("Blah")
 })
+})
+
